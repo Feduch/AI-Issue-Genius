bcf7751e97e256e4d8510f41809898f2bdd83e65
solar <fedor.buzinov@gmail.com>
2025-08-23 14:37:09 +0300

    Added db

--- server/prepare_database_db.sql
+++ server/prepare_database_db.sql
@@ -1,3 +1,25 @@
-CREATE DATABASE logs;
-CREATE USER logs WITH PASSWORD 'logs';
-ALTER USER logs WITH SUPERUSER;
+-- Создаем базу данных
+CREATE DATABASE ai_issue_genius;
+
+-- Создаем пользователя
+CREATE USER ai_issue_genius WITH PASSWORD 'ai_issue_genius';
+
+-- Даем пользователю права на базу данных
+GRANT ALL PRIVILEGES ON DATABASE ai_issue_genius TO ai_issue_genius;
+
+-- Подключаемся к созданной базе данных
+\c ai_issue_genius;
+
+-- Создаем таблицу в правильной базе данных
+CREATE TABLE logs (
+    id BIGSERIAL PRIMARY KEY,
+    timestamp TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    service VARCHAR(100) NOT NULL,
+    log JSONB,
+    ai_analysis JSONB,
+    analysis_time TIMESTAMP WITH TIME ZONE
+);
+
+-- Даем права пользователю на таблицу
+GRANT ALL PRIVILEGES ON TABLE logs TO ai_issue_genius;
+GRANT ALL PRIVILEGES ON SEQUENCE logs_id_seq TO ai_issue_genius;
